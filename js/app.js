081f24e2158021ddb5e8818895294ea772e61487
Tariqul Islam <usukuk@gmail.com>
2022-03-11 21:24:13 +0600

    show relevant heading on swithing tab

--- js/app.js
+++ js/app.js
@@ -36,19 +36,19 @@ const displayContent = (text) => {
 
 const switchTab = (id) => {
   if (id === "posts") {
-    document.getElementById("posts").style.display = "grid";
-    document.getElementById("liked").style.display = "none";
-    document.getElementById("reported").style.display = "none";
+    document.getElementById("posts-container").style.display = "grid";
+    document.getElementById("liked-posts-container").style.display = "none";
+    document.getElementById("reported-posts-container").style.display = "none";
   } else if (id === "liked") {
-    document.getElementById("liked").style.display = "block";
-    document.getElementById("posts").style.display = "none";
-    document.getElementById("reported").style.display = "none";
+    document.getElementById("liked-posts-container").style.display = "block";
+    document.getElementById("posts-container").style.display = "none";
+    document.getElementById("reported-posts-container").style.display = "none";
 
     displayLikedPosts();
   } else {
-    document.getElementById("reported").style.display = "block";
-    document.getElementById("posts").style.display = "none";
-    document.getElementById("liked").style.display = "none";
+    document.getElementById("reported-posts-container").style.display = "block";
+    document.getElementById("posts-container").style.display = "none";
+    document.getElementById("liked-posts-container").style.display = "none";
 
     displayReportedPosts();
   }
